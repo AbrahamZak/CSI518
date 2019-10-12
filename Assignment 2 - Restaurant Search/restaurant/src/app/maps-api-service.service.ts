73cbf82c415675c8644f80dc714043190a2cc8dd
Abraham Zakharov <abrahamzakharov@gmail.com>
2019-10-12 18:27:13 -0400

    added GET request

--- Assignment 2 - Restaurant Search/restaurant/src/app/maps-api-service.service.ts
+++ Assignment 2 - Restaurant Search/restaurant/src/app/maps-api-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapsApiServiceService {
 
-  constructor() { }
+  API_KEY:string ="AIzaSyAURLmSdQ6xkXOlp5Bv0D-kDJmHgZUGUOQ";
+  location:string = "Albany"
+  constructor(private httpClient: HttpClient) { }
+  public getRestaurants(){
+    return this.httpClient.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=$(this.location}+in+11229&key=${this.API_KEY}');
+  }
 }
